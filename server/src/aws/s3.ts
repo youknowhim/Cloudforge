0ff783e00b64373e120e9d24e5208ef9dbb4f56f
Pallav Rai <143748569+youknowhim@users.noreply.github.com>
2026-08-15 18:05:41 +0530

    s3 env key vaue changed as per env

--- server/src/aws/s3.ts
+++ server/src/aws/s3.ts
@@ -11,7 +11,7 @@ const s3 = new S3Client({
   region: process.env.AWS_REGION,
 
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+    accessKeyId: process.env.AWS_ACCESS_KEY!,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
   },
 });
