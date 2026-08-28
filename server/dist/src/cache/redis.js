9414a266bc52804623babbf7d3b96779a3cad626
Pallav Rai <143748569+youknowhim@users.noreply.github.com>
2026-08-28 16:15:41 +0530

    deleted manually created dist

--- server/dist/src/cache/redis.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const redis_1 = require("@upstash/redis");
-const redis = new redis_1.Redis({
-    url: process.env.UPSTASH_REDIS_REST_URL,
-    token: process.env.UPSTASH_REDIS_REST_TOKEN,
-});
-exports.default = redis;
